f4dc84283df4b245667c511164e98b29dba57682
Ahmad Amer Bakran <ahmbak01@easv365.dk>
2023-05-28 16:20:27 +0200

    added some comments

--- test/easv_2nd_term_exam/util/PictureUtilityTest.java
+++ test/easv_2nd_term_exam/util/PictureUtilityTest.java
@@ -6,18 +6,19 @@ import javafx.scene.image.WritableImage;
 import javafx.scene.paint.Color;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+/**
+ * Tests the methods in the PictureUtility class.
+ */
 public class PictureUtilityTest {
 
-    private static final String TEST_IMAGE_PATH = "src/easv_2nd_term_exam/gui/views/images_resource/Logo_WUAV.png";
-
-
+    /**
+     * Tests the imageToByteArray and byteArrayToImage methods in the PictureUtility class.
+     * This is done by creating a 1x1 pixel image, converting it to a byte array, and then converting it back to an image.
+     * The original and final images are then compared to ensure they are equal.
+     */
     @Test
     public void testImageToByteArrayAndBack() {
         // Create a 1x1 pixel image
@@ -40,17 +41,5 @@ public class PictureUtilityTest {
         PixelReader originalReader = originalImage.getPixelReader();
         PixelReader finalReader = finalImage.getPixelReader();
         assertEquals(originalReader.getColor(0, 0), finalReader.getColor(0, 0));
-    }
-
-    @Test
-    public void testByteArrayToImage() throws IOException {
-        // Load the test image file into a byte array
-        byte[] imageBytes = Files.readAllBytes(Paths.get(TEST_IMAGE_PATH));
-
-        // Convert the byte array back to an image
-        Image image = PictureUtility.byteArrayToImage(imageBytes);
-
-        // Verify the image is not null
-        assertNotNull(image);
     }
 }
